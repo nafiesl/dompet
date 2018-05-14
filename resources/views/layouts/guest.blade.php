a2a3bf85ebf106c6b6120154196abb2722104425
Nafies Luthfi <nafiesl@gmail.com>
2018-05-14 12:12:06 +0800

    Update welcome view
    Use bootstrap jumbotron-narrow template example

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield ('title') - {{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/guest.css') }}" rel="stylesheet">
+    @yield('styles')
+</head>
+<body>
+    <div class="container">
+        <div class="header clearfix">
+            @if (Route::has('login'))
+            <nav>
+                <ul class="nav nav-pills pull-right">
+                   @auth
+                       <li role="presentation" class="active"><a href="{{ url('/home') }}">Home</a></li>
+                   @else
+                        <li role="presentation" class="active"><a href="{{ route('login') }}">Login</a></li>
+                        <li role="presentation"><a href="{{ route('register') }}">Register</a></li>
+                   @endauth
+                </ul>
+            </nav>
+            @endif
+            <h3 class="text-muted">{{ config('app.name', 'Laravel') }}</h3>
+        </div>
+
+        @yield ('content')
+
+        <footer class="footer">
+            <p>&copy; 2018 Nafies Luthfi.</p>
+        </footer>
+
+    </div> <!-- /container -->
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,31 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.guest')
 
-        <title>Laravel</title>
+@section('title', trans('app.welcome'))
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+<div class="jumbotron">
+    <h1>{{ config('app.name', 'Laravel') }}</h1>
+    <p class="lead">Dompet is a personal bookkeeping web application, built with Laravel 5.5. It designed for easy bookkeeping for personal income and spending. </p>
+    <p><a class="btn btn-lg btn-success" href="{{ route('register') }}" role="button">Sign up today</a></p>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<div class="row marketing">
+    <div class="row">
+        <div class="col-lg-4">
+            <h4>Objective</h4>
+            <p>Easy bookkeeping for personal income and spending (amount of money).</p>
         </div>
-    </body>
-</html>
+        <div class="col-lg-8">
+            <h4>Concept</h4>
+            <p>To aquire our objective, we need this features on the application:</p>
+            <ul>
+                <li>User can register.</li>
+                <li>User can see transaction history by date.</li>
+                <li>User add transactions for income and spending.</li>
+                <li>User can categorize the transaction.</li>
+                <li>User can see transaction summary on each month or a year.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
