59c8099e0e2d479dc2eb65f0c84f76df91a2e2e7
Nafies Luthfi <nafiesl@gmail.com>
2018-05-14 13:36:34 +0800

    Update top-nav design

--- resources/views/layouts/partials/top-nav.blade.php
+++ resources/views/layouts/partials/top-nav.blade.php
@@ -1,53 +1,32 @@
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
             </a>
         </div>
 
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @else
-                    <li>{{ link_to_route('transactions.index', __('transaction.transaction')) }}</li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+        <!-- Right Side Of Navbar -->
+        <div class="nav navbar-nav navbar-right" style="margin: 0;">
+            <!-- Authentication Links -->
+            @guest
+                {{ link_to_route('login', __('auth.login'), [], ['class' => 'xs-navbar']) }}
+                {{ link_to_route('register', __('auth.register'), [], ['class' => 'xs-navbar']) }}
+            @else
+                {{ link_to_route('transactions.index', __('transaction.transaction'), [], ['class' => 'xs-navbar']) }}
+                {{ link_to_route('password.change', __('auth.change_password'), [], ['class' => 'xs-navbar']) }}
+                <a class="xs-navbar" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+                </a>
 
-                        <ul class="dropdown-menu">
-                            <li>{{ link_to_route('password.change', __('auth.change_password')) }}</li>
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    <input type="submit" value="{{ __('auth.logout') }}" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @endguest
-            </ul>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    <input type="submit" value="{{ __('auth.logout') }}" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endguest
         </div>
     </div>
 </nav>
