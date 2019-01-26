b949573a60893e5d13b2b81c2eef8fbc86e5d998
Nafies Luthfi <nafiesl@gmail.com>
2019-01-26 21:09:25 +0800

    Add en/report lang and fix category en lang

---
+++ resources/lang/en/report.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'report'            => 'Report',
+    'view_yearly_label' => 'View Yearly',
+    'view_report'       => 'View Report',
+    'this_year'         => 'This Year',
+    'graph'             => 'Summary Graph',
+    'detail'            => 'Report Detail',
+    'view_monthly'      => 'View Monthly',
+];
