11f86e450736be0c09dcda3cf724256756065c91
Nafies Luthfi <nafiesl@gmail.com>
2019-01-27 07:47:52 +0800

    Disable selected lang

--- resources/views/layouts/partials/lang_switcher.blade.php
+++ resources/views/layouts/partials/lang_switcher.blade.php
@@ -1,2 +1,2 @@
-{!! FormField::formButton(['route' => 'lang.switch', 'method' => 'patch', 'title' => __('app.switch_en_lang')], 'en', ['class' => 'btn btn-default btn-xs navbar-btn', 'id' => 'lang_en'], ['lang' => 'en']) !!}
-{!! FormField::formButton(['route' => 'lang.switch', 'method' => 'patch', 'title' => __('app.switch_id_lang')], 'id', ['class' => 'btn btn-default btn-xs navbar-btn', 'id' => 'lang_id'], ['lang' => 'id']) !!}
+{!! FormField::formButton(['route' => 'lang.switch', 'method' => 'patch', 'title' => __('app.switch_en_lang')], 'en', ['class' => 'btn btn-default btn-xs navbar-btn', 'id' => 'lang_en'] + (config('app.locale') == 'en' ? ['disabled' => 'disabled'] : []), ['lang' => 'en']) !!}
+{!! FormField::formButton(['route' => 'lang.switch', 'method' => 'patch', 'title' => __('app.switch_id_lang')], 'id', ['class' => 'btn btn-default btn-xs navbar-btn', 'id' => 'lang_id'] + (config('app.locale') == 'id' ? ['disabled' => 'disabled'] : []), ['lang' => 'id']) !!}
