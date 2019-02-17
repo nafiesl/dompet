c34705c04b892ac56c8c4c45a71247218ae44333
Nafies Luthfi <nafiesl@gmail.com>
2019-02-17 19:39:28 +0800

    Add transaction summary below transaction list

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -157,10 +157,36 @@
                         @endphp
                         <a href="{{ $categoryRoute }}">{!! optional($transaction->category)->name_label !!}</a>
                     </div>
-                    @if (!$loop->last)
-                        <hr style="margin: 6px 0">
-                    @endif
+                    <hr style="margin: 6px 0">
                 @endforeach
+                <div class="row">
+                    <div class="col-xs-6 text-right">{{ __('transaction.start_balance') }}</div>
+                    <div class="col-xs-6 text-right">
+                        @if ($transactions->last())
+                            {{ format_number(balance(Carbon\Carbon::parse($transactions->last()->date)->subDay()->format('Y-m-d'))) }}
+                        @else
+                            0
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 text-right">{{ __('transaction.income_total') }}</div>
+                    <div class="col-xs-6 text-right">{{ format_number($incomeTotal) }}</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 text-right">{{ __('transaction.spending_total') }}</div>
+                    <div class="col-xs-6 text-right">{{ format_number($spendingTotal) }}</div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 text-right">{{ __('transaction.end_balance') }}</div>
+                    <div class="col-xs-6 text-right">
+                        @if ($transactions->first())
+                            {{ format_number(balance($transactions->first()->date)) }}
+                        @else
+                            0
+                        @endif
+                    </div>
+                </div>
             </div>
             @enddesktop
         </div>
