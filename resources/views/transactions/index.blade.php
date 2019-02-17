ab0bcfcbf36b45a170fb3eb6b286930b8e8bfba1
Nafies Luthfi <nafiesl@gmail.com>
2019-02-17 19:23:40 +0800

    Change transaction listing format for mobile view

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -15,6 +15,7 @@
             <div class="panel-heading">
                 @include('transactions.partials.index_filters')
             </div>
+            @desktop
             <table class="table table-condensed table-bordered">
                 <thead>
                     <tr>
@@ -117,6 +118,49 @@
                     </tr>
                 </tfoot>
             </table>
+            @elsedesktop
+            <div class="panel-body">
+                @foreach ($transactions as $transaction)
+                    <span class="pull-right">{{ $transaction->amount_string }}</span>
+                    {{ link_to_route('transactions.index', $transaction->date, [
+                        'date' => $transaction->date_only,
+                        'month' => $month,
+                        'year' => $year,
+                        'category_id' => request('category_id'),
+                    ]) }}
+                    <div>
+                        {{ $transaction->description }}
+                        @can('update', $transaction)
+                            {!! link_to_route(
+                                'transactions.index',
+                                __('app.edit'),
+                                ['action' => 'edit', 'id' => $transaction->id] + request(['month', 'year', 'query', 'category_id']),
+                                ['id' => 'edit-transaction-'.$transaction->id, 'class' => 'pull-right text-danger']
+                            ) !!}
+                        @endcan
+                    </div>
+                    <div>
+                        @php
+                            $partnerRoute = route('partners.show', [
+                                $transaction->partner_id,
+                                'start_date' => $year.'-'.$month.'-01',
+                                'end_date' => $year.'-'.$month.'-'.date('t'),
+                            ]);
+                        @endphp
+                        <a href="{{ $partnerRoute }}">{!! optional($transaction->partner)->name_label !!}</a>
+                        @php
+                            $categoryRoute = route('categories.show', [
+                                $transaction->category_id,
+                                'start_date' => $year.'-'.$month.'-01',
+                                'end_date' => $year.'-'.$month.'-'.date('t'),
+                            ]);
+                        @endphp
+                        <a href="{{ $categoryRoute }}">{!! optional($transaction->category)->name_label !!}</a>
+                    </div>
+                    <hr style="margin: 6px 0">
+                @endforeach
+            </div>
+            @enddesktop
         </div>
     </div>
     <div class="col-md-4">
