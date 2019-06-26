845e549a6d61b1295a255bbbc3238898ea48da0d
Kang Dels <crushfire2004@gmail.com>
2019-06-26 15:56:10 +0700

    Fix delete request test

--- tests/Unit/Requests/Categories/DeleteRequestTest.php
+++ tests/Unit/Requests/Categories/DeleteRequestTest.php
@@ -21,8 +21,9 @@ class DeleteRequestTest extends TestCase
     public function it_fails_for_empty_attributes()
     {
         $this->assertValidationFails(new CategoryDeleteRequest(), [], function ($errors) {
-            $this->assertCount(1, $errors);
+            $this->assertCount(2, $errors);
             $this->assertEquals(__('validation.required'), $errors->first('category_id'));
+            $this->assertEquals(__('validation.required'), $errors->first('delete_transactions'));
         });
     }
 
@@ -30,6 +31,7 @@ class DeleteRequestTest extends TestCase
     {
         return array_merge([
             'category_id' => '1',
+            'delete_transactions' => '1',
         ], $overrides);
     }
 }
