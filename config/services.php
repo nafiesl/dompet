0058007efcbf24c2ea72c8f5557caf7d2355dfc8
Satya Kresna <satyakresna6295@gmail.com>
2019-08-09 22:09:11 +0800

    Simplify passport login endpoint

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     ],
 
     'passport' => [
-        'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
+        'login_endpoint' => env('APP_URL')."".env('PASSPORT_LOGIN_ENDPOINT'),
         'client_id' => env('PASSPORT_CLIENT_ID'),
         'client_secret' => env('PASSPORT_CLIENT_SECRET')
     ]
