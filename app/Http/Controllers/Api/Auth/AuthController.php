9dd54eeba5e658c5b1c8a48092bec7c2c2453ef8
Satya Kresna <satyakresna6295@gmail.com>
2020-01-19 21:16:14 +0800

    feat: add validation in login Passport API

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\Api\Auth;
 use Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email'    => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ]);
+    }
+
     public function login(Request $request)
     {
+        $this->validator($request->all())->validate();
+
         $form = [
             'grant_type' => 'password',
             'client_id' => config('services.passport.client_id'),
@@ -32,6 +49,8 @@ class AuthController extends Controller
             $token->revoke();
         });
 
-        return response()->json('Logged out successfully', 200);
+        return response()->json(array(
+            "message" => 'Logged out successfully'
+        ), 200);
     }
 }
