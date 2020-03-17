80784a59c23100742cb98a7fee4332f448f1dd87
Satya Kresna <satyakresna6295@gmail.com>
2020-03-17 20:20:52 +0800

    feat(navbar): display links based on logged in user

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,17 @@
 <header class="shadow bg-white p-4">
   <nav class="md:mx-16 flex items-center justify-between">
     <a href="/">Dompet</a>
-    <a href="/login" class="border-2 border-black border-solid rounded p-2">Masuk</a>
+    {{-- Todo: replace login link with logout link when user logged in --}}
+    @guest
+      <a href="/login" class="border-2 border-black border-solid rounded p-2">Masuk</a>
+    @else
+      <button onclick="event.preventDefault();document.getElementById('logout-form').submit();" 
+        class="border-2 border-black border-solid rounded p-2">Logout</button>
+
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          <input type="submit" value="{{ __('auth.logout') }}" style="display: none;">
+          {{ csrf_field() }}
+      </form>
+    @endguest
   </nav>
 </header>
