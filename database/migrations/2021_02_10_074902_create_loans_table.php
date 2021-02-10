a008277972b267e4c03127faa07942bd72908701
Nafies Luthfi <nafiesl@gmail.com>
2021-02-10 12:31:54 +0800

    Update loan table migration

--- database/migrations/2021_02_10_074902_create_loans_table.php
+++ database/migrations/2021_02_10_074902_create_loans_table.php
@@ -14,12 +14,18 @@ class CreateLoansTable extends Migration
     public function up()
     {
         Schema::create('loans', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name', 60);
+            $table->increments('id');
+            $table->unsignedInteger('partner_id');
+            $table->unsignedTinyInteger('in_out');
+            $table->unsignedDecimal('amount', 12);
+            $table->unsignedTinyInteger('planned_payment_count')->default(1);
             $table->string('description')->nullable();
-            $table->unsignedBigInteger('creator_id');
+            $table->unsignedInteger('creator_id');
+            $table->date('started_date')->nullable();
+            $table->date('closed_date')->nullable();
             $table->timestamps();
 
+            $table->foreign('partner_id')->references('id')->on('partners')->onDelete('restrict');
             $table->foreign('creator_id')->references('id')->on('users')->onDelete('restrict');
         });
     }
