fef698b7b179a04eb22a8aa5ee8f2fbe5f2a6ba6
Nafies Luthfi <nafiesl@gmail.com>
2021-02-15 21:46:39 +0800

    Remove unused route

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,6 @@ Route::group(['middleware' => 'auth'], function () {
      * Loans Routes
      */
     Route::resource('loans', 'LoanController');
-    Route::get('loans/{loan}/transactions/create', 'Loans\TransactionController@create')
-        ->name('loans.transactions.create');
     Route::post('loans/{loan}/transactions', 'Loans\TransactionController@store')
         ->name('loans.transactions.store');
 
