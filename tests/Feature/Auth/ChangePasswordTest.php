9ba493704b9c1fd505ca0203eaf03355514a8092
Nafies Luthfi <nafiesl@gmail.com>
2021-02-17 22:04:10 +0800

    Update some helper functions

--- tests/Feature/Auth/ChangePasswordTest.php
+++ tests/Feature/Auth/ChangePasswordTest.php
@@ -14,17 +14,17 @@ class ChangePasswordTest extends TestCase
     {
         $user = $this->loginAsUser();
 
-        $this->visit(route('home'));
-        $this->click(trans('settings.settings'));
-        $this->click(trans('auth.change_password'));
+        $this->visitRoute('home');
+        $this->click(__('settings.settings'));
+        $this->click(__('auth.change_password'));
 
-        $this->submitForm(trans('auth.change_password'), [
+        $this->submitForm(__('auth.change_password'), [
             'old_password'          => 'secret',
             'password'              => 'rahasia',
             'password_confirmation' => 'rahasia',
         ]);
 
-        $this->see(trans('auth.password_changed'));
+        $this->see(__('auth.password_changed'));
 
         $this->assertTrue(
             app('hash')->check('rahasia', $user->password),
@@ -37,17 +37,17 @@ class ChangePasswordTest extends TestCase
     {
         $user = $this->loginAsUser();
 
-        $this->visit(route('home'));
-        $this->click(trans('settings.settings'));
-        $this->click(trans('auth.change_password'));
+        $this->visitRoute('home');
+        $this->click(__('settings.settings'));
+        $this->click(__('auth.change_password'));
 
-        $this->submitForm(trans('auth.change_password'), [
+        $this->submitForm(__('auth.change_password'), [
             'old_password'          => 'member1',
             'password'              => 'rahasia',
             'password_confirmation' => 'rahasia',
         ]);
 
-        $this->see(trans('auth.old_password_failed'));
+        $this->see(__('auth.old_password_failed'));
 
         $this->assertTrue(
             app('hash')->check('secret', $user->password),
