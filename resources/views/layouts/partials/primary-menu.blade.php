8d9f00fb2256fa16d31c870e258e0906e417fa77
Nafies Luthfi <nafiesl@gmail.com>
2021-02-23 22:10:49 +0800

    Prepare top menu

---
+++ resources/views/layouts/partials/primary-menu.blade.php
@@ -0,0 +1,13 @@
+<div class="col-lg order-lg-first">
+    <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+        <li class="nav-item">
+            <a href="{{ route('home') }}" class="nav-link {{ Request::routeIs('home') ? 'active' : '' }}"><i class="fe fe-home"></i> Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('transactions.index') }}" class="nav-link {{ Request::segment(1) == 'transactions' ? 'active' : '' }}"><i class="fe fe-truck"></i> {{ __('transaction.transaction') }}</a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('loans.index') }}" class="nav-link {{ Request::segment(1) == 'loans' ? 'active' : '' }}"><i class="fe fe-truck"></i> {{ __('loan.loan') }}</a>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/layouts/partials/top-nav-right.blade.php
@@ -0,0 +1,22 @@
+<div class="dropdown">
+    <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
+        <span class="avatar avatar-placeholder"></span>
+        <span class="ml-2 d-none d-lg-block">
+            <span class="text-default">{{ auth()->user()->name }}</span>
+        </span>
+    </a>
+    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+        <a class="dropdown-item" href="{{ route('profile.show') }}">
+            <i class="dropdown-icon fe fe-user"></i> {{ __('auth.profile') }}
+        </a>
+        <a class="dropdown-item" href="{{ route('password.change') }}">
+            <i class="dropdown-icon fe fe-lock"></i> {{ __('passwords.change_password') }}
+        </a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="dropdown-icon fe fe-log-out"></i> {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+    </div>
+</div>
