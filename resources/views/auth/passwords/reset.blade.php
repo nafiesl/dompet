332a683fc8566250fad28424004c07aedffd2dd6
Nafies Luthfi <nafiesl@gmail.com>
2021-03-17 22:05:23 +0800

    Update guest layout

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 @section('title', __('auth.reset_password'))
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,45 +1,39 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Language" content="en" />
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#4188c9">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield ('title') - {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/guest.css') }}" rel="stylesheet">
-    @yield('styles')
+    <link rel="icon" href="./favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
 </head>
 <body>
-    <div class="container">
-        <div class="header clearfix">
-            @if (Route::has('login'))
-            <nav>
-                <ul class="nav nav-pills float-right">
-                   @auth
-                       <li role="presentation" class="active"><a href="{{ url('/home') }}">Home</a></li>
-                   @else
-                        <li role="presentation" class="active"><a href="{{ route('login') }}">Login</a></li>
-                        <li role="presentation"><a href="{{ route('register') }}">Register</a></li>
-                   @endauth
-                </ul>
-            </nav>
-            @endif
-            <h3 class="text-muted">{{ config('app.name', 'Laravel') }}</h3>
+    <div class="page">
+        <div class="page-single">
+            <div class="container">
+                @yield('content')
+            </div>
         </div>
-
-        @yield ('content')
-
-        <footer class="footer">
-            <p>&copy; 2018 Nafies Luthfi.</p>
-        </footer>
-
-    </div> <!-- /container -->
+    </div>
+    <script src="{{ asset('js/app.js') }}" ></script>
+    @include('layouts.partials.noty')
 </body>
 </html>
