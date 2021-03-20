ce4c8379f4476a17a5373170565a00b23faeef6b
Nafies Luthfi <nafiesl@gmail.com>
2021-03-20 21:51:41 +0800

    Update user profile page

--- resources/views/auth/profile/show.blade.php
+++ resources/views/auth/profile/show.blade.php
@@ -4,17 +4,17 @@
 
 @section('content_settings')
 <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title">@yield('title')</h3></div>
-            <table class="table table-sm panel-table">
+    <div class="col-md-6 offset-md-3">
+        <div class="card">
+            <div class="card-header"><h3 class="card-title">@yield('title')</h3></div>
+            <table class="table table-sm card-table">
                 <tbody>
                     <tr><td>{{ __('user.name') }}</td><td>{{ $user->name }}</td></tr>
                     <tr><td>{{ __('user.email') }}</td><td>{{ $user->email }}</td></tr>
                     <tr><td>{{ __('user.account_start_date') }}</td><td>{{ $user->account_start_date }}</td></tr>
                 </tbody>
             </table>
-            <div class="panel-footer">
+            <div class="card-footer">
                 <a href="{{ route('profile.edit') }}" class="btn btn-success">{{ __('user.profile_edit') }}</a>
             </div>
         </div>
