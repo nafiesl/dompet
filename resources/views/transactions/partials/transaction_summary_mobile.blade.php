1afa14c3171c7ac5e810fe410a5e2d6504da6751
Nafies Luthfi <nafiesl@gmail.com>
2021-03-21 14:26:28 +0800

    Fix mobile view on the transaction list page

--- resources/views/transactions/partials/transaction_summary_mobile.blade.php
+++ resources/views/transactions/partials/transaction_summary_mobile.blade.php
@@ -1,20 +1,20 @@
 @if (request('category_id') || request('partner_id'))
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.income_total') }}</div>
-        <div class="col-xs-6 text-right strong">{{ format_number($incomeTotal) }}</div>
+        <div class="col-6 text-right strong">{{ __('transaction.income_total') }}</div>
+        <div class="col-6 text-right strong">{{ format_number($incomeTotal) }}</div>
     </div>
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.spending_total') }}</div>
-        <div class="col-xs-6 text-right strong">{{ format_number($spendingTotal) }}</div>
+        <div class="col-6 text-right strong">{{ __('transaction.spending_total') }}</div>
+        <div class="col-6 text-right strong">{{ format_number($spendingTotal) }}</div>
     </div>
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.end_balance') }}</div>
-        <div class="col-xs-6 text-right strong">{{ number_format($incomeTotal - $spendingTotal, 2) }}</div>
+        <div class="col-6 text-right strong">{{ __('transaction.end_balance') }}</div>
+        <div class="col-6 text-right strong">{{ number_format($incomeTotal - $spendingTotal, 2) }}</div>
     </div>
 @else
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.start_balance') }}</div>
-        <div class="col-xs-6 text-right strong">
+        <div class="col-6 text-right strong">{{ __('transaction.start_balance') }}</div>
+        <div class="col-6 text-right strong">
             @php
                 $balance = 0;
             @endphp
@@ -26,16 +26,16 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.income_total') }}</div>
-        <div class="col-xs-6 text-right strong">{{ format_number($incomeTotal) }}</div>
+        <div class="col-6 text-right strong">{{ __('transaction.income_total') }}</div>
+        <div class="col-6 text-right strong">{{ format_number($incomeTotal) }}</div>
     </div>
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.spending_total') }}</div>
-        <div class="col-xs-6 text-right strong">{{ format_number($spendingTotal) }}</div>
+        <div class="col-6 text-right strong">{{ __('transaction.spending_total') }}</div>
+        <div class="col-6 text-right strong">{{ format_number($spendingTotal) }}</div>
     </div>
     <div class="row">
-        <div class="col-xs-6 text-right strong">{{ __('transaction.end_balance') }}</div>
-        <div class="col-xs-6 text-right strong">
+        <div class="col-6 text-right strong">{{ __('transaction.end_balance') }}</div>
+        <div class="col-6 text-right strong">
             @if ($transactions->first())
                 {{ format_number($balance + $incomeTotal - $spendingTotal) }}
             @else
