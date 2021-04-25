4d1ad72b35a1acc07a10c5ce7a25aa345f01d92c
Nafies Luthfi <nafiesl@gmail.com>
2021-04-25 23:29:34 +0800

    Fix navigation for mobile

--- resources/views/layouts/partials/top_nav_bs_4_test.blade.php
+++ resources/views/layouts/partials/top_nav_bs_4_test.blade.php
@@ -10,9 +10,9 @@
                 @endguest
             </a>
         </div>
-        <div class="nav navbar-nav mx-2 d-flex">
+        <div class="nav navbar-nav mx-2 flex-row">
             @auth
-            <a href="{{ route('home') }}" class="xs-navbar" title="{{ __('transaction.current_balance') }}">
+            <a href="{{ route('home') }}" class="xs-navbar mr-4" title="{{ __('transaction.current_balance') }}">
                 <img src="{{ asset('images/icons8-coins-16.png') }}" alt=""> {{ format_number(balance(date('Y-m-d'))) }}
             </a>
             @include ('layouts.partials.lang_switcher')
@@ -20,25 +20,25 @@
         </div>
 
         <!-- Right Side Of Navbar -->
-        <div class="nav navbar-nav ml-auto d-sm-block">
+        <div class="nav navbar-nav ml-auto d-block mt-3 mt-md-0">
             <!-- Authentication Links -->
-            <a class="xs-navbar mr-3" href="{{ route('transactions.index') }}" title="{{ __('transaction.transaction') }}">
+            <a class="xs-navbar mr-4" href="{{ route('transactions.index') }}" title="{{ __('transaction.transaction') }}">
                 <i class="fe fe-repeat"></i>
                 <span class="d-none d-sm-inline">{{ __('transaction.transaction') }}</span>
             </a>
-            <a class="xs-navbar mr-3" href="{{ route('loans.index') }}" title="{{ __('loan.loan') }}">
+            <a class="xs-navbar mr-4" href="{{ route('loans.index') }}" title="{{ __('loan.loan') }}">
                 <i class="fe fe-refresh-cw"></i>
                 <span class="d-none d-sm-inline">{{ __('loan.loan') }}</span>
             </a>
-            <a class="xs-navbar mr-3" href="{{ route('reports.index') }}" title="{{ __('report.report') }}">
+            <a class="xs-navbar mr-4" href="{{ route('reports.index') }}" title="{{ __('report.report') }}">
                 <i class="fe fe-activity"></i>
                 <span class="d-none d-sm-inline">{{ __('report.report') }}</span>
             </a>
-            <a class="xs-navbar mr-3" href="{{ route('profile.show') }}" title="{{ __('settings.settings') }}">
+            <a class="xs-navbar mr-4" href="{{ route('profile.show') }}" title="{{ __('settings.settings') }}">
                 <i class="fe fe-settings"></i>
                 <span class="d-none d-sm-inline">{{ __('settings.settings') }}</span>
             </a>
-            <a class="xs-navbar mr-3" href="{{ route('logout') }}"
+            <a class="xs-navbar mr-4" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
                 <i class="fe fe-log-out"></i>
