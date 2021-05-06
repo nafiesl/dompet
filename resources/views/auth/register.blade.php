1b1bed42a25d24522fb8064e24b073fa603c68fd
Nafies Luthfi <nafiesl@gmail.com>
2021-05-06 23:20:34 +0800

    Update the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,74 +4,62 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8 offset-md-2">
+        <div class="col col-login mx-auto">
             <div class="card">
                 <div class="card-header"><h3 class="card-title">{{ __('auth.register') }}</h3></div>
 
                 <div class="card-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">{{ __('app.name') }}</label>
+                            <label for="name" class="form-label">{{ __('app.name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{ __('auth.email') }}</label>
+                            <label for="email" class="form-label">{{ __('auth.email') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">{{ __('auth.new_password') }}</label>
+                            <label for="password" class="form-label">{{ __('auth.new_password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">{{ __('auth.new_password_confirmation') }}</label>
+                            <label for="password-confirm" class="form-label">{{ __('auth.new_password_confirmation') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('auth.register') }}
-                                </button>
+                            <button type="submit" class="btn btn-primary btn-block">
+                                {{ __('auth.register') }}
+                            </button>
 
-                                <a class="btn btn-link" href="{{ route('login') }}">
-                                    {{ __('auth.have_account') }}
-                                </a>
-                            </div>
+                            {{ link_to_route('login', __('auth.have_account'), [], ['class' => 'btn btn-link btn-block']) }}
                         </div>
                     </form>
                 </div>
