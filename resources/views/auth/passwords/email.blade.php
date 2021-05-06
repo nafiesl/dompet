f20bad9703b53a0cc6718eb28494d0120c88535d
Nafies Luthfi <nafiesl@gmail.com>
2021-05-06 23:23:38 +0800

    Update the reset password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-8 mx-auto">
             <div class="card">
-                <div class="card-header"><h3 class="card-title">{{ __('auth.reset_password') }}</h3></div>
+                <div class="card-header"><div class="card-title">{{ __('auth.reset_password') }}</div></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,26 +18,9 @@
                     <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
                         @csrf
                         {!! FormField::email('email', ['label' => __('auth.email')]) !!}
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{ __('auth.email') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('auth.send_reset_password_link') }}
-                                </button>
-                            </div>
+                            {{ Form::submit(__('auth.send_reset_password_link'), ['class' => 'btn btn-primary']) }}
                         </div>
                     </form>
                 </div>
