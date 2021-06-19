2c768105a56fde2ebc3854d5b945a25f546fec8f
Nafies Luthfi <nafiesl@gmail.com>
2021-06-19 20:48:14 +0800

    Improve navigation for mobile view

--- resources/views/layouts/partials/top_nav.blade.php
+++ resources/views/layouts/partials/top_nav.blade.php
@@ -20,29 +20,29 @@
         </div>
 
         <!-- Right Side Of Navbar -->
-        <div class="nav navbar-nav ml-auto d-block mt-3 mt-md-0">
+        <div class="nav navbar-nav ml-auto d-none d-sm-block">
             <!-- Authentication Links -->
             <a class="xs-navbar mr-4" href="{{ route('transactions.index') }}" title="{{ __('transaction.transaction') }}">
-                <i class="fe fe-repeat h3 d-inline d-sm-none"></i>
-                <span class="d-none d-sm-inline"><i class="fe fe-repeat"></i> {{ __('transaction.transaction') }}</span>
+                <i class="fe fe-repeat h3 d-inline d-lg-none"></i>
+                <span class="d-none d-lg-inline"><i class="fe fe-repeat"></i> {{ __('transaction.transaction') }}</span>
             </a>
             <a class="xs-navbar mr-4" href="{{ route('loans.index') }}" title="{{ __('loan.loan') }}">
-                <i class="fe fe-refresh-cw h3 d-inline d-sm-none"></i>
-                <span class="d-none d-sm-inline"><i class="fe fe-refresh-cw"></i> {{ __('loan.loan') }}</span>
+                <i class="fe fe-refresh-cw h3 d-inline d-lg-none"></i>
+                <span class="d-none d-lg-inline"><i class="fe fe-refresh-cw"></i> {{ __('loan.loan') }}</span>
             </a>
             <a class="xs-navbar mr-4" href="{{ route('reports.index') }}" title="{{ __('report.report') }}">
-                <i class="fe fe-bar-chart-2 h3 d-inline d-sm-none"></i>
-                <span class="d-none d-sm-inline"><i class="fe fe-bar-chart-2"></i> {{ __('report.report') }}</span>
+                <i class="fe fe-bar-chart-2 h3 d-inline d-lg-none"></i>
+                <span class="d-none d-lg-inline"><i class="fe fe-bar-chart-2"></i> {{ __('report.report') }}</span>
             </a>
             <a class="xs-navbar mr-4" href="{{ route('profile.show') }}" title="{{ __('settings.settings') }}">
-                <i class="fe fe-settings h3 d-inline d-sm-none"></i>
-                <span class="d-none d-sm-inline"><i class="fe fe-settings"></i> {{ __('settings.settings') }}</span>
+                <i class="fe fe-settings h3 d-inline d-lg-none"></i>
+                <span class="d-none d-lg-inline"><i class="fe fe-settings"></i> {{ __('settings.settings') }}</span>
             </a>
             <a class="xs-navbar mr-4" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
-                 <i class="fe fe-log-out h3 d-inline d-sm-none"></i>
-                <i class="fe fe-log-out d-none d-sm-inline"></i>
+                 <i class="fe fe-log-out h3 d-inline d-lg-none"></i>
+                <i class="fe fe-log-out d-none d-lg-inline"></i>
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -52,3 +52,26 @@
         </div>
     </div>
 </nav>
+
+<!-- Mobile Navigation -->
+<nav class="navbar fixed-bottom navbar-light bg-white d-block d-sm-none">
+    <div class="row text-center small justify-content-center">
+        <a class="col" href="{{ route('transactions.index') }}" title="{{ __('transaction.transaction') }}">
+            <div><i class="fe fe-repeat h3"></i></div>
+            {{ __('transaction.transaction') }}
+        </a>
+        <a class="col" href="{{ route('loans.index') }}" title="{{ __('loan.loan') }}">
+            <div><i class="fe fe-refresh-cw h3"></i></div>
+            {{ __('loan.loan') }}
+        </a>
+        <a class="col" href="{{ route('reports.index') }}" title="{{ __('report.report') }}">
+            <div><i class="fe fe-bar-chart-2 h3"></i></div>
+            {{ __('report.report') }}
+        </a>
+        <a class="col" href="{{ route('profile.show') }}" title="{{ __('settings.settings') }}">
+            <div><i class="fe fe-settings h3"></i></div>
+            {{ __('settings.settings') }}
+        </a>
+    </div>
+</nav>
+
