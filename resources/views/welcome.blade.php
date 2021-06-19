b102e1fbf7bf45565c76b23e30b41ec62e7215b9
Nafies Luthfi <nafiesl@gmail.com>
2021-06-19 21:16:32 +0800

    Fix the welcome page structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,23 +12,21 @@
     </p>
 </div>
 
-<div class="row marketing">
-    <div class="row">
-        <div class="col-lg-4">
-            <h4>Objective</h4>
-            <p>Easy bookkeeping for personal income and spending (amount of money).</p>
-        </div>
-        <div class="col-lg-8">
-            <h4>Concept</h4>
-            <p>To aquire our objective, we need this features on the application:</p>
-            <ul>
-                <li>User can register.</li>
-                <li>User can see transaction history by date.</li>
-                <li>User add transactions for income and spending.</li>
-                <li>User can categorize the transaction.</li>
-                <li>User can see transaction summary on each month or a year.</li>
-            </ul>
-        </div>
+<div class="row">
+    <div class="col-lg-4">
+        <h4>Objective</h4>
+        <p>Easy bookkeeping for personal income and spending (amount of money).</p>
+    </div>
+    <div class="col-lg-8">
+        <h4>Concept</h4>
+        <p>To aquire our objective, we need this features on the application:</p>
+        <ul>
+            <li>User can register.</li>
+            <li>User can see transaction history by date.</li>
+            <li>User add transactions for income and spending.</li>
+            <li>User can categorize the transaction.</li>
+            <li>User can see transaction summary on each month or a year.</li>
+        </ul>
     </div>
 </div>
 @endsection
