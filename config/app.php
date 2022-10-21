5ef5b5896212a34693dce8f6ae6b2317e98bc540
Nafies Luthfi <nafiesl@gmail.com>
2022-10-21 22:46:09 +0800

    feat: Configurable timezone

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Jakarta',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
 
     /*
     |--------------------------------------------------------------------------
